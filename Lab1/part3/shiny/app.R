f8995adb19b3fca9184761bea9c0806917f28473
urbangeek <gupta.amlan@gmail.com>
2019-03-06 02:34:31 -0500

    shiny update

--- Lab1/part3/shiny/app.R
+++ Lab1/part3/shiny/app.R
@@ -6,17 +6,17 @@ counties <- readRDS("data/counties.rds")
 
 # User interface ----
 ui <- fluidPage(
-  titlePanel("censusVis"),
+  titlePanel("Data Intensive Computing: Lab 1: Part3"),
   
   sidebarLayout(
     sidebarPanel(
-      helpText("Create demographic maps with 
-               information from the 2010 US Census."),
+      helpText("Select Heatmap from below list"),
       
       selectInput("var", 
-                  label = "Choose a variable to display",
-                  choices = c("Percent White", "Percent Black",
-                              "Percent Hispanic", "Percent Asian"),
+                  label = "Choose:",
+                  choices = c("CDC Map" = "cdc",
+                              "Twitter Map" = "twt",
+                              "CDC vs Twitter MAp" = "cdctwt"),
                   selected = "Percent White"),
       
       sliderInput("range", 
@@ -24,23 +24,42 @@ ui <- fluidPage(
                   min = 0, max = 100, value = c(0, 100))
       ),
     
-    mainPanel(plotOutput("map"))
+    #mainPanel(plotOutput("map"))
+    mainPanel(imageOutput("myImage"))
+    
   )
   )
 
 server <- function(input, output) {
-  output$map <- renderPlot({
-    args <- switch(input$var,
-                   "Percent White" = list(counties$white, "darkgreen", "% White"),
-                   "Percent Black" = list(counties$black, "black", "% Black"),
-                   "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
-                   "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+#  output$map <- renderPlot({
+ #   args <- switch(input$var,
+#                   "Percent White" = list(counties$white, "darkgreen", "% White"),
+ #                  "Percent Black" = list(counties$black, "black", "% Black"),
+  #                 "Percent Hispanic" = list(counties$hispanic, "darkorange", "% Hispanic"),
+   #                "Percent Asian" = list(counties$asian, "darkviolet", "% Asian"))
+#    
+ #   args$min <- input$range[1]
+#    args$max <- input$range[2]
+ #   
+#    do.call(percent_map, args)
+#  })
+  
+  output$myImage <- renderImage({
+
     
-    args$min <- input$range[1]
-    args$max <- input$range[2]
+    switch(input$var,
+           "cdc" = list(src = 'data/tweets_by_state.png', contentType = 'image/png', width = 840, height = 840),
+           "twt" = list(src = 'data/ili_activity_level.png', contentType = 'image/png', width = 840, height = 840),
+           "cdctwt" = list(src = 'data/tweets_by_state.png', contentType = 'image/png', width = 840, height = 840)
+           
+    )
     
-    do.call(percent_map, args)
-  })
+    
+    
+  }, deleteFile = FALSE)
+  
+  
+  
 }
 
 # Run app ----
